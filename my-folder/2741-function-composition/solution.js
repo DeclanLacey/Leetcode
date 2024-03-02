55d7ae119af8d46537f5ea818efb544591da05ea
Declan Lacey <declan.lacey@hotmail.com>
2024-03-02 22:17:28 +0000

    [LeetCode Sync] Runtime - 57 ms (86.89%), Memory - 49.8 MB (85.92%)

--- my-folder/2741-function-composition/solution.js
+++ my-folder/2741-function-composition/solution.js
@@ -3,8 +3,18 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    return x => functions.reduceRight((acc, f) => f(acc), x)
+    
+    return function(x) {
+        let output = x
+        for(let i = functions.length - 1; i >= 0; i--){
+            let currentFunction = functions[i]
+            output = currentFunction(output)
+        }
+        return output
+    }
+    
 };
+
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
