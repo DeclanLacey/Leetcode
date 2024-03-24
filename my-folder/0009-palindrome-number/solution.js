b495c5103c521347dd6dcc08893d569f65d7bf36
Declan Lacey <declan.lacey@hotmail.com>
2024-03-24 19:32:17 +0000

    [LeetCode Sync] Runtime - 113 ms (94.61%), Memory - 59.1 MB (11.31%)

---
+++ my-folder/0009-palindrome-number/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let input = x
+    input = input.toString()
+
+    let xAsArray = []
+    let backwards = ""
+    for (let i = 0; i < input.length; i++) {
+        xAsArray.push(input[i])
+    }
+    xAsArray = xAsArray.reverse()
+
+    for (let i = 0; i < xAsArray.length; i++) {
+        backwards += xAsArray[i]
+    }
+   
+    if (input === backwards) {
+        return true
+    }else {
+        return false
+    }
+
+    
+};
+
