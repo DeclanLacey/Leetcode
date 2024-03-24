c9503a5948735b4cb20d4b05075dbcd054eb89d7
Declan Lacey <declan.lacey@hotmail.com>
2024-03-24 21:52:41 +0000

    [LeetCode Sync] Runtime - 55 ms (57.95%), Memory - 50.7 MB (26.43%)

---
+++ my-folder/0014-longest-common-prefix/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let currentPrefix = ""
+    let strings = strs.sort()
+    let noPrefix = false
+    console.log(strings)
+
+    for (let i = 0; i < strings[0].length; i++) {
+        for (let j = 0; j < strings[strings.length - 1].length; j++) {
+            if (strings[0][i] === strings[strings.length - 1][i]) {
+                currentPrefix += strings[0][i]
+                break
+            }else {
+                noPrefix = true
+                break
+            }
+        }
+        if (noPrefix) {
+            break
+        }
+    }
+    
+    return currentPrefix
+};
+
+
