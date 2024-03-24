3887b19d1722184d9d819b39905b0abca37da17b
Declan Lacey <declan.lacey@hotmail.com>
2024-03-24 19:17:08 +0000

    [LeetCode Sync] Runtime - 113 ms (21.89%), Memory - 49.3 MB (87.06%)

---
+++ my-folder/0001-two-sum/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let targetIndexs = []
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                targetIndexs.push(i)
+                targetIndexs.push(j)
+            }
+        }
+    }
+    return targetIndexs
+};
+
