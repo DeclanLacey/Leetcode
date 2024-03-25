4f4b0057ee7c83e99a8e2f36dec44a5fc21a0b1c
Declan Lacey <declan.lacey@hotmail.com>
2024-03-25 19:23:11 +0000

    [LeetCode Sync] Runtime - 61 ms (14.59%), Memory - 48.2 MB (95.30%)

---
+++ my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let firstFoundIndex = -1 
+
+    if (haystack.includes(needle) === false) {
+        return firstFoundIndex
+    }else {
+        firstFoundIndex = haystack.indexOf(needle)
+    }
+    
+    return firstFoundIndex
+    
+};
