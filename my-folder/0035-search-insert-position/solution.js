d3bfb8a92126594d01f6a7a407b399d7d45321a9
Declan Lacey <declan.lacey@hotmail.com>
2024-03-25 22:34:44 +0000

    [LeetCode Sync] Runtime - 61 ms (12.04%), Memory - 49.4 MB (16.33%)

---
+++ my-folder/0035-search-insert-position/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let index
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            index = i
+        }else if (nums[i] < target && nums[i + 1] > target) {
+            index = i + 1
+        }else if (i === nums.length - 1 && nums[i] < target) {
+            index = i + 1
+        }else if (i === 0 && nums[i] > target) {
+            index = i
+        }
+    }
+
+    return index
+};
