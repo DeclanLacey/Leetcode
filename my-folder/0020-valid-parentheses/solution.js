4e913a90cdc792529557eb7fede7b7018a67237a
Declan Lacey <declan.lacey@hotmail.com>
2024-03-25 17:12:51 +0000

    [LeetCode Sync] Runtime - 60 ms (43.03%), Memory - 50.7 MB (33.51%)

---
+++ my-folder/0020-valid-parentheses/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    let correctParens = ["()", "[]", "{}"]
+
+    for (let i = 0; i < s.length; i++) {
+        stack.push(s[i])
+
+        let lastTwo = stack[stack.length - 2] + stack[stack.length - 1]
+
+        if (correctParens.includes(lastTwo)) {
+            stack.pop()
+            stack.pop()
+        }
+    }
+
+    return stack.length === 0
+};
