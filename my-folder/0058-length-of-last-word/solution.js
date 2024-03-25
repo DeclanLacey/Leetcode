7dc70d8cad04ebfd03b6e92153a54ee55fd352ae
Declan Lacey <declan.lacey@hotmail.com>
2024-03-25 22:41:28 +0000

    [LeetCode Sync] Runtime - 51 ms (59.14%), Memory - 49.1 MB (29.79%)

---
+++ my-folder/0058-length-of-last-word/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let wordArray = []
+    wordArray = s.split(" ")
+
+    for (let i = 0; i < wordArray.length; i++) {
+        if (wordArray[i] === "") {
+            wordArray.splice(i, 1)
+            i--
+        }
+    }
+
+    return wordArray[wordArray.length - 1].length
+};
