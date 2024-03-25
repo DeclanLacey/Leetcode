be51163c0d69037cef02b58bf6805561d0cd3fea
Declan Lacey <declan.lacey@hotmail.com>
2024-03-25 23:19:18 +0000

    [LeetCode Sync] Runtime - 50 ms (69.48%), Memory - 48.7 MB (69.96%)

---
+++ my-folder/0066-plus-one/solution.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    if (digits[digits.length - 1] < 9) {
+        digits.splice((digits.length - 1), 1, digits[digits.length - 1] + 1)
+    }else {
+        let numberOfNines = 0
+        for (let i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] === 9) {
+                numberOfNines++
+            }else {
+                break
+            }
+        }
+        
+        if (numberOfNines === digits.length) {
+            digits.splice(0, digits.length, 1)
+            for (let i = 0; i < numberOfNines; i++) {
+                digits.push(0)
+            }
+        }else {
+            digits.splice(digits.length - numberOfNines, numberOfNines)
+            digits.splice(digits.length - 1, 1, digits[digits.length - 1] + 1)
+
+            for (let i = 0; i < numberOfNines; i++) {
+                digits.push(0)
+            }
+        }
+
+       
+    }
+
+    return digits 
+};
