efabc0332e789022acff874cb3e8ab5c6a362c84
Declan Lacey <declan.lacey@hotmail.com>
2024-03-26 23:16:36 +0000

    [LeetCode Sync] Runtime - 112 ms (8.98%), Memory - 53 MB (24.84%)

---
+++ my-folder/0069-sqrtx/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let subtractionNum = 1
+    let numberOfSubtractions = 0
+    let originalNum = x
+    while (originalNum >= 0) {
+    originalNum = originalNum - subtractionNum
+    numberOfSubtractions++
+    subtractionNum += 2
+    }
+     return numberOfSubtractions - 1
+};
