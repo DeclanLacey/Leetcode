b8a4301e7674af9158cfb9f6733c9031487cac64
Declan Lacey <declan.lacey@hotmail.com>
2024-03-27 00:02:19 +0000

    [LeetCode Sync] Runtime - 52 ms (48.19%), Memory - 48.6 MB (64.59%)

---
+++ my-folder/0070-climbing-stairs/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+
+    let fib = [1, 1]
+    
+    if (n === 1) {
+        return 1
+    }else {
+        for(let i = 2; i <= n; i++) {
+            fib[i] = fib[i - 2] + fib [i - 1]
+            if (i === n) {
+                return fib[i]
+            }
+        }
+    }
+    
+};
