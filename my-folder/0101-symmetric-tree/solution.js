a89826e8b5ddaeaa37bacd85fd293d2bab8c552f
Declan Lacey <declan.lacey@hotmail.com>
2024-03-30 17:23:58 +0000

    [LeetCode Sync] Runtime - 48 ms (97.66%), Memory - 51.4 MB (72.98%)

---
+++ my-folder/0101-symmetric-tree/solution.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+
+    let areSym = true
+    
+    function traverse(node1, node2) {
+
+        if (!node1 && !node2) {
+            return;
+        }
+
+        if (!node1 || !node2 || node1.val !== node2.val) {
+            areSym = false;
+            return;
+        }
+
+        traverse(node1.left, node2.right)
+        traverse(node1.right, node2.left)
+
+    }
+
+    traverse(root, root)
+
+    return areSym
+
+};
