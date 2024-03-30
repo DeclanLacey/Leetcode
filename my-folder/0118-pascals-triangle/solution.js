fbc4a09708a313f9977dfe9be7b08231d40d0fe7
Declan Lacey <declan.lacey@hotmail.com>
2024-03-30 22:55:14 +0000

    [LeetCode Sync] Runtime - 59 ms (53.59%), Memory - 50.7 MB (36.93%)

---
+++ my-folder/0118-pascals-triangle/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let output = [[1], [1,1]]
+
+    if (numRows === 1) {
+        output = [[1]]
+    }
+    for (let i = 1; i < numRows - 1; i++) {
+        let newRow = [1,1]
+        for (let j = 0; j < output[i].length - 1; j++) {
+            newRow.splice(j + 1, 0, output[i][j] + output[i][j + 1])
+        } 
+        output.push(newRow)
+
+    }
+    return output
+};
