59f0cb2007212aaff186fadcf9116e4f50d50cf0
Declan Lacey <declan.lacey@hotmail.com>
2024-03-30 16:23:22 +0000

    [LeetCode Sync] Runtime - 52 ms (60.63%), Memory - 49.1 MB (69.65%)

---
+++ my-folder/0100-same-tree/solution.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    let treeOne = []
+    let treeTwo = []
+    let areSameTree = false
+
+    function traverseNode(node, treeArray) {
+        if (node === null) {
+            return
+        }
+
+        treeArray.push(node.val)
+
+        if (node.left) {
+            traverseNode(node.left, treeArray)
+        }else if (node.left === null) {
+            treeArray.push(null)
+        }
+
+        treeArray.push(node.val)
+
+        if (node.right) {
+            traverseNode(node.right, treeArray)
+        }else if (node.right === null) {
+            treeArray.push(null)
+        }
+
+        treeArray.push(node.val)
+    }
+
+    traverseNode(p, treeOne)
+    traverseNode(q, treeTwo)
+
+    if (treeOne.length === treeTwo.length) {
+        if (treeOne.length === 0) {
+            areSameTree = true
+        }else {
+            for (let i = 0; i < treeOne.length; i++) {
+                if (treeOne[i] === treeTwo[i]) {
+                    areSameTree = true
+                    continue
+                }else {
+                    areSameTree = false
+                    break
+                }
+            }
+        }
+    }
+
+    return areSameTree
+
+};
