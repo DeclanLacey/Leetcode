bc5903768dc559d489ca1642b6dc4a250361df7b
Declan Lacey <declan.lacey@hotmail.com>
2024-03-31 15:59:33 +0000

    [LeetCode Sync] Runtime - 82 ms (22.73%), Memory - 58.7 MB (83.93%)

---
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+    let buyPrice = prices[0]
+    let maxProfit = 0
+
+    for (let i = 1; i < prices.length; i++) {
+        buyPrice = Math.min(buyPrice, prices[i])
+        maxProfit = Math.max(prices[i] - buyPrice,  maxProfit)
+    }
+
+    return maxProfit
+};
