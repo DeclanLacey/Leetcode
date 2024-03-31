36705eb2292c9bdd2cc943ef3e13748d9f759fb3
Declan Lacey <declan.lacey@hotmail.com>
2024-03-31 21:48:03 +0000

    [LeetCode Sync] Runtime - 64 ms (80.76%), Memory - 50.7 MB (92.57%)

---
+++ my-folder/0242-valid-anagram/solution.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+   
+    if (s.length != t.length) {
+        return false
+    }
+
+    let hashTableOne = {}
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in hashTableOne) {
+            hashTableOne[s[i]] = hashTableOne [s[i]] + 1
+        }else {
+            hashTableOne[s[i]] = 1
+        }
+        
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (t[i] in hashTableOne) {
+            hashTableOne[t[i]] = hashTableOne[t[i]] - 1
+        }
+
+        if (hashTableOne[t[i]] === 0) {
+            delete hashTableOne[t[i]]
+        }
+        
+    }
+
+    if (JSON.stringify(hashTableOne) === "{}") {
+        return true
+    }else {
+        return false
+    }
+
+};
