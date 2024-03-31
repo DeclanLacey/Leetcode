768598733d46b95ef26aac693108f09fb7d92f1d
Declan Lacey <declan.lacey@hotmail.com>
2024-03-31 16:45:19 +0000

    [LeetCode Sync] Runtime - 84 ms (7.49%), Memory - 57.9 MB (8.94%)

---
+++ my-folder/0125-valid-palindrome/solution.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+    let lowerCaseString = s.toLowerCase()
+    let alphaOnlyString = ""
+    let backwardsStringArray = []
+    let alphaOnlyStringBackwards = ""
+
+    for (let i = 0; i < lowerCaseString.length; i++) {
+        if (/[a-z0-9]/.test(lowerCaseString[i])) {
+            alphaOnlyString += lowerCaseString[i]
+        }
+    }
+    
+    for (let i = 0; i < alphaOnlyString.length; i++) {
+        backwardsStringArray.push(alphaOnlyString[i])
+    }
+
+    backwardsStringArray.reverse()
+
+    for (let i = 0; i < backwardsStringArray.length; i++) {
+        alphaOnlyStringBackwards += backwardsStringArray[i]
+    }
+
+    if (alphaOnlyString === alphaOnlyStringBackwards) {
+        return true
+    }else {
+        return false
+    }
+
+};
