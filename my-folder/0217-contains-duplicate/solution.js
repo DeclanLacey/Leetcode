d434ce6480118f517ab86bc6d5c8d4e1d93c906f
Declan Lacey <declan.lacey@hotmail.com>
2024-03-31 19:26:03 +0000

    [LeetCode Sync] Runtime - 88 ms (30.81%), Memory - 61.5 MB (56.30%)

---
+++ my-folder/0217-contains-duplicate/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+
+    let hashTable = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] in hashTable) {
+            return true
+        }
+        hashTable[nums[i]] = true
+    }
+
+    return false
+};
