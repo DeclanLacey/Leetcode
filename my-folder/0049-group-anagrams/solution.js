9f2a46f24b31d1a49ed5563c35567be0f65a2cb5
Declan Lacey <declan.lacey@hotmail.com>
2024-03-31 22:31:52 +0000

    [LeetCode Sync] Runtime - 125 ms (33.10%), Memory - 63.1 MB (54.19%)

---
+++ my-folder/0049-group-anagrams/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+
+    let strings = strs.map((str) => str.split("").sort().join(""))
+   
+    let results = {}
+
+    for (let i = 0; i < strings.length; i++) {
+        if (strings[i] in results) {
+            results[strings[i]].push(strs[i])
+        }else {
+            results[strings[i]] = [strs[i]]
+        }
+    }
+
+    return Object.values(results)
+
+
+};
