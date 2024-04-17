b804ece214d0c617d9a286a2a1545183a52b235e
Declan Lacey <declan.lacey@hotmail.com>
2024-04-17 00:24:27 +0000

    [LeetCode Sync] Runtime - 65 ms (75.09%), Memory - 53.6 MB (66.79%)

---
+++ my-folder/0347-top-k-frequent-elements/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let numsObj = {}
+    let numsArray = []
+    let resultArray = []
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] in numsObj) {
+            numsObj[nums[i]] += 1;
+        }else {
+            numsObj[nums[i]] = 1
+        }
+    }
+
+    for (let num in numsObj) {
+        numsArray.push([num, numsObj[num]])
+    }
+
+    numsArray.sort((a, b) => b[1] - a[1])
+
+    for (let i = 0; i < k; i++) {
+        resultArray.push(parseInt(numsArray[i][0]))
+    }
+
+    return resultArray
+};
