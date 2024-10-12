0559589a0ee246d4e7af6d6fdd1b3e9dcbc73235
Declan Lacey <declan.lacey@hotmail.com>
2024-10-12 19:43:44 +0000

    [LeetCode Sync] Runtime - 63 ms (40.47%), Memory - 52 MB (50.81%)

--- my-folder/0104-maximum-depth-of-binary-tree/solution.js
+++ my-folder/0104-maximum-depth-of-binary-tree/solution.js
@@ -11,25 +11,9 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-
-    function traverse(node) {
-        if (node === null) {
-            return 0
-        }else {
-
-            let rightDepth = traverse(node.right)
-            let leftDepth = traverse(node.left)
-
-            if (rightDepth > leftDepth) {
-                return rightDepth + 1
-            }else {
-                return leftDepth + 1
-            }
-        }
+    if (root === null) {
+        return 0;
     }
 
-    return traverse(root)
-
-   
-
+    return 1 + Math.max(maxDepth(root.right), maxDepth(root.left))
 };
