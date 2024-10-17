bf8ca6d853dc4a4a5c320cb3c09f149da9f93a84
Declan Lacey <declan.lacey@hotmail.com>
2024-10-17 19:40:28 +0000

    [LeetCode Sync] Runtime - 9 ms (100.00%), Memory - 56.2 MB (15.26%)

--- my-folder/0125-valid-palindrome/solution.js
+++ my-folder/0125-valid-palindrome/solution.js
@@ -3,32 +3,36 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    
-    let lowerCaseString = s.toLowerCase()
-    let alphaOnlyString = ""
-    let backwardsStringArray = []
-    let alphaOnlyStringBackwards = ""
+    let string = s.split("").filter((char) => isAlphaNumeric(char)).join("").toLowerCase()
+    let palindrome = true
+    let indexOne = 0
+    let indexTwo = string.length - 1
 
-    for (let i = 0; i < lowerCaseString.length; i++) {
-        if (/[a-z0-9]/.test(lowerCaseString[i])) {
-            alphaOnlyString += lowerCaseString[i]
+    while (true) {
+
+        if (indexOne > indexTwo) {
+            break
         }
-    }
-    
-    for (let i = 0; i < alphaOnlyString.length; i++) {
-        backwardsStringArray.push(alphaOnlyString[i])
-    }
 
-    backwardsStringArray.reverse()
-
-    for (let i = 0; i < backwardsStringArray.length; i++) {
-        alphaOnlyStringBackwards += backwardsStringArray[i]
-    }
-
-    if (alphaOnlyString === alphaOnlyStringBackwards) {
-        return true
-    }else {
-        return false
+        if (string[indexOne] === string[indexTwo]) {
+            indexOne += 1
+            indexTwo -= 1
+            continue
+        }else if (string[indexOne] != string[indexTwo]) {
+            palindrome = false
+            break
+        }
+        
     }
 
+    function isAlphaNumeric(char) {
+        const code = char.charCodeAt(0);
+        return (
+            (code >= 48 && code <= 57) || // 0-9
+            (code >= 65 && code <= 90) || // A-Z
+            (code >= 97 && code <= 122) // a-z
+        );
+    }
+
+    return palindrome
 };
