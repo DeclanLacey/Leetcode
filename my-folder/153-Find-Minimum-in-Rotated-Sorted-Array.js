28bf81b038724608d9c8ff1e71671c5a940a0ce0
Declan Lacey <declan.lacey@hotmail.com>
2024-10-20 17:44:10 -0500

    [153] [Time Beats: 97.38%] [Memory Beats: 5.08%] - LeetPush

--- my-folder/153-Find-Minimum-in-Rotated-Sorted-Array.js
+++ my-folder/153-Find-Minimum-in-Rotated-Sorted-Array.js
@@ -3,11 +3,24 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i + 1] < nums[i]) {
-            return nums[i + 1]
-        }if (i === nums.length - 1) {
-            return nums[0]
+    let left = 0
+        let right = nums.length - 1
+
+        while (left < right) {
+            const mid = (left + right) >> 1
+            const midNum = nums[mid]
+            const leftNum = nums[left]
+            const rightNum = nums[right]
+
+            
+            console.log(\Left: \, leftNum)
+            console.log(\Mid: \, midNum)
+            console.log(\Right: \, rightNum)
+
+            if (leftNum < rightNum) return leftNum
+            if (leftNum <= midNum) left = mid + 1
+            if (midNum < leftNum) right = mid
         }
-    }
+
+        return nums[left]
 };
