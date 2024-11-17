4e1fc5ec9a71aae38562a4362257acb3ff4e1642
Declan Lacey <declan.lacey@hotmail.com>
2024-11-17 17:10:47 -0600

    [242] [Time Beats: 87.21%] [Memory Beats: 71.68%] - LeetPush

--- my-folder/242-Valid-Anagram.js
+++ my-folder/242-Valid-Anagram.js
@@ -4,13 +4,22 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let stringOne = s.split("").sort().join()
-    let stringTwo = t.split("").sort().join()
+    if (s.length !== t.length) return false
 
-    if (stringOne == stringTwo) {
+        const countOne = {}
+        const countTwo = {}
+
+        for (let i = 0; i < s.length; i++) {
+            countOne[s[i]] = (countOne[s[i]] || 0) + 1
+            countTwo[t[i]] = (countTwo[t[i]] || 0) + 1
+        }
+
+        for (const key in countOne) {
+            if (countOne[key] !== countTwo[key]) {
+                return false
+            }
+        }
+
         return true
-    }else {
-        return false
-    }
         
 };
