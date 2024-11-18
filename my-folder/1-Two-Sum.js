5a317b388db3fa573ed33ea25ce4cefcb5eb264d
Declan Lacey <declan.lacey@hotmail.com>
2024-11-17 20:40:25 -0600

    [1] [Time Beats: 59.41%] [Memory Beats: 8.54%] - LeetPush

--- my-folder/1-Two-Sum.js
+++ my-folder/1-Two-Sum.js
@@ -4,15 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = new Map()
+    const numMap = new Map()
 
-    for (let i = 0; i < nums.length; i++) {
-        let num = nums[i]
-        let remainingTarget = target - num
-        let sumIndex = map.get(remainingTarget)
+      for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i]
+        if (numMap.has(diff)) {
+            return [numMap.get(diff), i]
+        }
+        
 
-        if (sumIndex || sumIndex === 0) return [i, sumIndex]
+        numMap.set(nums[i], i)
+      }
 
-        map.set(num, i)
-    }
+      return []
 };
