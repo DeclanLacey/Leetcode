3b6d52a99633013ba05c3b0ae3a80923a802ff7a
Declan Lacey <declan.lacey@hotmail.com>
2024-11-20 17:34:13 -0600

    [347] [Time Beats: 89.52%] [Memory Beats: 89.19%] - LeetPush

--- my-folder/347-Top-K-Frequent-Elements.js
+++ my-folder/347-Top-K-Frequent-Elements.js
@@ -4,26 +4,26 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let numsCount = {}
-        let numSet = new Set()
+    let numMap = new Map()
+        let numArray = []
         let res = []
 
-        for (let num of nums) {
-            if (numSet.has(num))  {
-                numsCount[num] += 1
+        for (let i = 0; i < nums.length; i++) {
+            if (numMap.has(nums[i])) {
+                let currentValue = numMap.get(nums[i])
+                numMap.set(nums[i], currentValue + 1)
             }else {
-                numSet.add(num)
-                numsCount[num] = 1
+                numMap.set(nums[i], 1)
             }
         }
 
-        const entries = Object.entries(numsCount);
-        entries.sort((a, b) => b[1] - a[1])
+        numArray = [...numMap.entries()]
+        numArray.sort((a, b) => b[1] - a[1])
 
         for (let i = 0; i < k; i++) {
-            res.push(Number(entries[i][0]))
+            res.push(numArray[i][0])
         }
-        
+
         return res
 
 };
