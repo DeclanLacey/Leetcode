a031056cb1e342baa09c8b8cad5cb8fbdd7315ba
Declan Lacey <declan.lacey@hotmail.com>
2024-12-08 17:41:49 -0600

    [121] [Time Beats: 53.18%] [Memory Beats: 43.39%] - LeetPush

--- my-folder/121-Best-Time-to-Buy-and-Sell-Stock.js
+++ my-folder/121-Best-Time-to-Buy-and-Sell-Stock.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = prices[0]
-        let max = min
-        let value = 0
+    let maxProfit = 0
+    let left = 0;
 
-        for (let i = 0; i < prices.length; i++) {
-            if (i != prices.length - 1 && prices[i] <= min) {
-                max = min = prices[i]
-            }else if (prices[i] > max) {
-                max = prices[i]
-            }
-
-            value = max - min > value ? max - min : value;
+    for (let right = 0; right < prices.length; right++) {
+        if (prices[right] < prices[left]) {
+            left = right
         }
 
-        return value
+        if (prices[right] - prices[left] > maxProfit) {
+            maxProfit = prices[right] - prices[left] 
+        }
+    }
+    return maxProfit
 };
